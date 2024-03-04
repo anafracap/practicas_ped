aa39970ab46278bda5d8c5b70915f70fd6e943ae
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2024-03-04 15:46:34 +0100

    Refactor bolos

--- p1/source/bolos.py
+++ p1/source/bolos.py
@@ -1,24 +1,23 @@
 class Partida():
-    global contador
     def iniciar_partida(self):
         return None
 
     def jugar_ronda(self):
-        global contador
-        if contador == 10:
+        global num_ronda
+        if num_ronda == 10:
             raise Exception('NoJuegesMas')
-        contador = contador + 1
+        num_ronda = num_ronda + 1
         return None
 
     def esta_terminada_la_partida(self):
-        global contador
-        if contador < 10:
+        global num_ronda
+        if num_ronda < 10:
             return False
-        if contador == 10: 
+        if num_ronda == 10: 
             return True
 
 
     def __init__(self):
-        global contador
-        contador = 0
+        global num_ronda
+        num_ronda = 0
         return None
