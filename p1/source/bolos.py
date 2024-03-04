256752e65c2b8f4209b19db095c0a5adfb54fac4
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2024-03-04 16:16:09 +0100

    Pasado test 4

--- p1/source/bolos.py
+++ p1/source/bolos.py
@@ -2,7 +2,7 @@ class Partida():
     def iniciar_partida(self):
         return None
 
-    def jugar_ronda(self):
+    def jugar_ronda(self, turno1 = None, turno2 = None):
         ronda = self._num_ronda
         if ronda == 10:
             raise Exception('NoJuegesMas')
@@ -16,7 +16,7 @@ class Partida():
         if self._num_ronda == 10: 
             return True
 
-    def ver_contador():
+    def ver_contador(self):
         return 10
 
 
