bf44e0a3dd85e3ccda39608fc3a39409c820c68b
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2024-03-08 08:34:17 +0100

    todavia no funnciona

--- p1/source/bolos.py
+++ p1/source/bolos.py
@@ -2,12 +2,14 @@ class Partida():
     def iniciar_partida(self):
         return None
 
-    def jugar_ronda(self, turno1 = None, turno2 = None):
+    def jugar_ronda(self, turno1, turno2 = None):
         ronda = self._num_ronda
         if ronda == 10:
             raise Exception('NoJuegesMas')
-        self._num_ronda = ronda + 1
-        return None
+        else:
+            self._num_ronda = ronda + 1
+            self._contador = self._contador + turno1 + turno2
+            return None
 
     def esta_terminada_la_partida(self):
         self._num_ronda
@@ -17,7 +19,7 @@ class Partida():
             return True
 
     def ver_contador(self):
-        return 10
+        return sel._contador
 
 
     def __init__(self):
--- p1/source/test.py
+++ p1/source/test.py
@@ -6,23 +6,23 @@ class TestClass(unittest.TestCase):
         partida = Partida()
         partida.iniciar_partida()
         for i in range(10):
-            partida.jugar_ronda()
+            partida.jugar_ronda(0,0)
         self.assertTrue(partida.esta_terminada_la_partida())
 
     def test_partida_no_terminada(self):
         partida = Partida()
         partida.iniciar_partida()
-        partida.jugar_ronda()
-        partida.jugar_ronda()
+        partida.jugar_ronda(0,0)
+        partida.jugar_ronda(0,0)
         self.assertFalse(partida.esta_terminada_la_partida())
 
     def test_te_has_pasado_de_rondas(self):
         partida = Partida()
         partida.iniciar_partida()
         for i in range(10):
-            partida.jugar_ronda()
+            partida.jugar_ronda(0,0)
         with pytest.raises(Exception, match='NoJuegesMas'):
-            partida.jugar_ronda()
+            partida.jugar_ronda(0,0)
 
     def test_contador_ronda_abierta(self):
         partida = Partida()
@@ -30,4 +30,11 @@ class TestClass(unittest.TestCase):
         for i in range(10):
             partida.jugar_ronda(0,1)
         self.assertEqual(partida.ver_contador(), 10)
+
+    def test_contador_ronda_abierta_mejor(self):
+        partida = Partida()
+        partida.iniciar_partida()
+        for i in range(10):
+            partida.jugar_ronda(2,0)
+        self.assertEqual(partida.ver_contador(), 20)
 
