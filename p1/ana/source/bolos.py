602a35a65fad02325b2edaeda50acc78a428228b
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2024-03-12 16:00:11 +0100

    Otro refactor (fallará con semiplenos)

--- p1/ana/source/bolos.py
+++ p1/ana/source/bolos.py
@@ -2,33 +2,36 @@ class Partida():
     def iniciar_partida(self):
         return None
 
-    def jugar_ronda(self, turno1, turno2 = 0):
+    def jugar_ronda(self, turno1, turno2 = None):
         ronda = self._num_ronda
         if ronda == 10:
             raise Exception('NoJuegesMas')
+        self._tirar_ronda(turno1)
+        if turno2 != None:
+            self._tirar_ronda(turno2)
+        self._num_ronda = ronda + 1
+       
+
+    def _tirar_ronda(self, turno):
         if self._tiradas_a_sumar > 0:
-            if isinstance(turno1, str):
-                turno1 = 10
-            self._sumar_bonus(turno1)
-        if self._tiradas_a_sumar > 0:
-            self._sumar_bonus(turno2)
-        if isinstance(turno1, str) or isinstance(turno2, str):
-            self._calcular_tiradas_raras(turno1, turno2)
+            if turno == 'X':
+                turno = 10
+            self._sumar_bonus(turno)
+        if isinstance(turno, str):
+            self._calcular_tiradas_raras(turno)
         else:
-            self._num_ronda = ronda + 1
-            self._contador = self._contador + turno1 + turno2
-            return None
+            self._contador = self._contador + turno
 
     def _sumar_bonus(self, turno):
         self._tiradas_a_sumar = self._tiradas_a_sumar - 1       
         self._contador = self._contador + turno
 
-    def _calcular_tiradas_raras(self, turno1, turno2):
-        if turno1 == 'X':
+    def _calcular_tiradas_raras(self, turno):
+        if turno == 'X':
             self._tiradas_a_sumar = 2
             self._contador = self._contador + 10
             return None
-        if turno2 == '/':
+        if turno == '/':
             self._tiradas_a_sumar = 1
             self._contador = self._contador + 10
             return None
