b7c32436ae5f0cf751002da2ace3ea24dd16e061
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2024-04-11 20:16:31 +0200

    Funciona aplicacion 2

---
+++ p2/ana/aplicacion2/cliente_servidor_hora.py
@@ -0,0 +1,27 @@
+import os, sys, datetime
+
+sys.argv[0] = "cliserv2"
+
+rdC,wdS = os.pipe()         # son file descriptors, no file objects - de servidor a cliente
+
+pid = os.fork() 
+
+if pid:                   # padre - servidor
+    sys.argv[0] = "serv2"
+    os.close(rdC)
+    print (pid)
+
+    date = datetime.datetime.now().strftime('%c')
+    
+    os.write(wdS, date.encode('utf8'))
+
+else:                     # hijo - cliente
+    sys.argv[0] = "cli2"
+    os.close(wdS) 
+    print(pid)
+
+    while True:
+        line = os.read(rdC, 100).decode('utf8').strip()
+        if not line:
+            break
+        print(line)
