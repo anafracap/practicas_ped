d0e839b3067bdfbf0005d963790cc3508a22861e
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2024-04-22 14:07:51 +0200

    P4, ap2, funciona con bucle

--- p4/ap2/cliente_hora.py
+++ p4/ap2/cliente_hora.py
@@ -6,11 +6,11 @@ server_address = "/tmp/ped4_p4_ap2_server.sock"
 
 pid = str(os.getpid())
 
-client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-
-client_socket.connect(server_address)
-client_socket.send(pid.encode())
-data = client_socket.recv(1024)
-print(data.decode())
+for i in range(10):
+    client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    client_socket.connect(server_address)
+    client_socket.send(pid.encode())
+    data = client_socket.recv(1024)
+    print(data.decode())
 
 client_socket.close()
