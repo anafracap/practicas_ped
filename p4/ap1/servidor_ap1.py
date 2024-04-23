453bf0f88c92ee9f39beec4cdd248045058973ca
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2024-04-23 15:39:15 +0200

    P4, ap1, servidor con forks

--- p4/ap1/servidor_ap1.py
+++ p4/ap1/servidor_ap1.py
@@ -1,5 +1,23 @@
 import os, sys, socket
 
+def answer_back(connection):
+    path_bytes = connection.recv(1024)
+    path = path_bytes.decode().strip()
+
+    file_size = os.path.getsize(path)
+    file_size_bytes = file_size.to_bytes(8, byteorder='big')
+    connection.send(file_size_bytes)
+
+    with open(path, 'rb') as fileO:  
+        while True:
+            content = fileO.read()
+            if not content:
+                fileO.close()
+                break
+            connection.send(content)
+    connection.close()
+
+
 sys.argv[0] = "serv2"
 
 server_address = "/tmp/ped4_p4_ap1_server.sock"
@@ -17,22 +35,15 @@ server_socket.listen() #cambiar para varias conexiones al tiempo
 try: 
     while True:
         connection, client_address = server_socket.accept()
+        pid = os.fork()
 
-        path_bytes = connection.recv(1024)
-        path = path_bytes.decode().strip()
-
-        file_size = os.path.getsize(path)
-        file_size_bytes = file_size.to_bytes(8, byteorder='big')
-        connection.send(file_size_bytes)
-
-        with open(path, 'rb') as fileO:  
-            while True:
-                content = fileO.read()
-                if not content:
-                    fileO.close()
-                    break
-                connection.send(content)
-                connection.close()
+        if pid == 0:  # Child process
+            server_socket.close()
+            answer_back(connection)
+            os._exit(0)
+        else:
+            connection.close()
+   
 except KeyboardInterrupt:
     os.write(2, b"Keyboard interrupt received. Exiting server.")
 
