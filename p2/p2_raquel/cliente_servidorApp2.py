1b7820e793746d54ccd9d3fdf7802151660d6525
raquelcerezocarrion <raquel.cerezocarrion>
2024-05-06 17:44:29 +0200

    Version4 no ejecuta, hago cambios a v5 Ficheros P2

--- p2/p2_raquel/cliente_servidorApp2.py
+++ p2/p2_raquel/cliente_servidorApp2.py
@@ -1,50 +1,52 @@
 import os
 import sys
+from multiprocessing import Process, Pipe
 
-def servidor():
-    # Cerrar los extremos que no se utilizan para evitar errores
-    sys.argv[0] = "servidor"
-    os.close(wdC)
-    os.close(rdC)
+def servidor(conn):
+    # Cerrar el extremo de escritura para evitar errores
+    conn.close()
     
-    data = os.read(rdS, 100).decode('utf8').strip()
+    data = conn.recv().decode('utf8').strip()
     print("Servidor: Recibido path del archivo:", data)
     try:
         with open(data, 'r') as file:
             contenido = file.read()
-            os.write(wdS, contenido.encode('utf8'))
+            conn.send(contenido.encode('utf8'))
             print("Servidor: Enviando contenido del archivo al cliente")
     except FileNotFoundError:
         error_msg = "Error: El archivo no existe"
-        os.write(wdS, error_msg.encode('utf8'))
+        conn.send(error_msg.encode('utf8'))
         print("Servidor: Archivo no encontrado, enviando mensaje de error")
 
-def cliente():
-    # Cerrar los extremos que no se utilizan para evitar errores
-    sys.argv[0] = "cliente"
-    os.close(rdS)
-    os.close(wdS)
-
-    # Obtener la ruta del archivo del primer argumento pasado desde la línea de comandos
-    ruta_archivo = sys.argv[1]
+def cliente(conn, ruta_archivo):
+    # Cerrar el extremo de lectura para evitar errores
+    conn.close()
 
     # Enviar el path del archivo al servidor
-    os.write(wdC, ruta_archivo.encode('utf8'))
+    conn.send(ruta_archivo.encode('utf8'))
     print("Cliente: Enviando path del archivo al servidor:", ruta_archivo)
 
     # Leer la respuesta del servidor y mostrarla en la salida estándar
-    response = os.read(rdC, 1000).decode('utf8')
+    response = conn.recv().decode('utf8')
     print("Cliente: Respuesta del servidor recibida:", response)
     sys.stdout.write(response)
 
 if __name__ == "__main__":
     # Crear las tuberías
-    rdS, wdS = os.pipe()  # Cliente a servidor
-    rdC, wdC = os.pipe()  # Servidor a cliente
-    
-    pid = os.fork()
+    servidor_conn, cliente_conn = Pipe()
 
-    if pid:  # Proceso padre (servidor)
-        servidor()
-    else:  # Proceso hijo (cliente)
-        cliente()
+    # Obtener la ruta del archivo del primer argumento pasado desde la línea de comandos
+    ruta_archivo = sys.argv[1]
+
+    # Crear procesos para el cliente y el servidor
+    servidor_process = Process(target=servidor, args=(servidor_conn,))
+    cliente_process = Process(target=cliente, args=(cliente_conn, ruta_archivo))
+
+    # Iniciar los procesos
+    servidor_process.start()
+    cliente_process.start()
+
+    # Esperar a que los procesos terminen
+    servidor_process.join()
+    cliente_process.join()
+
