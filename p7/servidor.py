f708c5bcd91250d1d849ccb1c4cf6b4a4add8f86
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2024-05-11 12:07:58 +0200

    P7, refactor server a clase

--- p7/servidor.py
+++ p7/servidor.py
@@ -1,120 +1,122 @@
 import os, sys, socket, select
 
-def send_all_clients(message):
-    for nick, c in clients.items():
-        try:
-            send_to_one(message, c)
-        except:
-            disconnect(nick)
+class Chat_server:
 
-def send_to_chat(message, nick):
-    if chats[nick] in groups:
-        for client in groups[chats[nick]]:
-            send_to_one(message, client)
-    else:
-        private = chats[nick][len('p'):]
-        if private in clients:
-            send_to_one(message, private)
+    def start(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.server_address, self.server_port))
+        self.server_socket.listen()
+        print(server_socket, file=sys.stderr)
 
-def sent_to_one(message, nick):
-    clients[nick].send(message.encode('utf-8'))
+        try: 
+            while True:
+                readable, _, _ = select.select([self.server_socket] + list(self.clients.values()), [], [])
 
-def verify_nick(cli_sock):
-    identify = "Please enter your UNIQUE nickname: "
-    cli_sock.send(identify.encode('utf-8'))
-    nick = cli_sock.recv(1024).decode('utf-8')
-    if nick in clients:
-        message = "Your nickname is already in use, unable to log in \n"
-        sent_to_one(message, nick)
-        cli_sock.close()
-        return False
-    else:
-        clients[nick] = cli_sock
-        print(list(clients), file=sys.stderr)
-        login = "You have successfully logged in! Type 'exit' to leave the chat.\n"
-        sent_to_one(login, nick)
-        chats[nick] = 'all'
-        groups[chats[nick]].add(nick)
-        text = f"{nick} has joined the chat!"
-        send_to_chat(text, nick)
-        return nick
+                for trigger_socket in readable:
+                    if trigger_socket == self.server_socket:
+                        client_socket, client_address = self.server_socket.accept()
+                        nick = self.verify_nick(client_socket)
+                        if nick:
+                            print(f"[*] Accepted connection from {client_address[0]}:{client_address[1]}", file=sys.stderr)
+                    else: # Receive message from existing client
+                        nick = [key for key, value in self.clients.items() if value == trigger_socket][0]
+                        message = trigger_socket.recv(1024).decode('utf-8')
+                        self.continue_conversation(message, nick)
 
-def continue_conversation(message, nick):
-    text = f"{nick}: {message}"
-    if message.lower() == 'exit':
-        disconnect(nick)
-    elif message.lower().startswith("group: "):
-        text = f"{nick} has left the chat!"
-        send_to_chat(text, nick)
-        group = message[len("group: "):]
-        if chats[nick] in groups:
-            groups[chats[nick]].remove(nick)
-        chats[nick] = group
-        if not group in groups:
-            groups[group] = set()
-        groups[group].add(nick)
-        message = f"You have joined the group {group}.\n"
-        send_to_one(message, nick)
-        text = f"{nick} has joined the chat!"
-        send_to_chat(text, nick)
-    elif message.lower().startswith("private: "):
-        text = f"{nick} has left the chat!"
-        send_to_chat(text, nick)
-        private = message[len("private: "):]
-        if chats[nick] in groups:
-            groups[chats[nick]].remove(nick)
-        chats[nick] = 'p' + private
-        message = f"You have joined a private chat with {private}.\n"
-        send_to_one(message, nick)
-        text = f"{nick} has joined a private chat with you.\n"
-        send_to_chat(text, nick)
-    else:
-        send_to_chat(text, nick)
+        except KeyboardInterrupt:
+            print("Keyboard interrupt received. Exiting server.", file=sys.stderr)
+        finally:
+            for nick in self.clients.copy():
+                self.disconnect(nick)
+            self.server_socket.close()
 
-def disconnect(nick):
-    if nick in clients:
-        text = f"{nick} has left the chat!"
-        send_to_chat(text, nick)
-        message = 'exit'
-        send_to_one(message, nick)
-        clients[nick].close()
-        if chats[nick] in groups:
-            groups[chats[nick]].remove(nick)
-        del chats[nick]
-        del clients[nick]
-        
+    def verify_nick(self, cli_sock):
+        identify = "Please enter your UNIQUE nickname: "
+        cli_sock.send(identify.encode('utf-8'))
+        nick = cli_sock.recv(1024).decode('utf-8')
+        if nick in self.clients:
+            message = "Your nickname is already in use, unable to log in \n"
+            self.send_to_one(message, nick)
+            cli_sock.close()
+            return False
+        else:
+            self.clients[nick] = cli_sock
+            print(list(self.clients), file=sys.stderr)
+            login = "You have successfully logged in! Type 'exit' to leave the chat.\n"
+            self.send_to_one(login, nick)
+            self.chats[nick] = 'all'
+            self.groups[chats[nick]].add(nick)
+            text = f"{nick} has joined the chat!"
+            self.send_to_chat(text, nick)
+            return nick
 
-server_address = "0.0.0.0"
-server_port = int(sys.argv[1])
+    def continue_conversation(self, message, nick):
+        text = f"{nick}: {message}"
+        if message.lower() == 'exit':
+            self.disconnect(nick)
+        elif message.lower().startswith("group: "):
+            text = f"{nick} has left the chat!"
+            self.send_to_chat(text, nick)
+            group = message[len("group: "):]
+            if self.chats[nick] in self.groups:
+                self.groups[self.chats[nick]].remove(nick)
+            self.chats[nick] = group
+            if not group in self.groups:
+                self.groups[group] = set()
+            self.groups[group].add(nick)
+            message = f"You have joined the group {group}.\n"
+            self.send_to_one(message, nick)
+            text = f"{nick} has joined the chat!"
+            self.send_to_chat(text, nick)
+        elif message.lower().startswith("private: "):
+            text = f"{nick} has left the chat!"
+            self.send_to_chat(text, nick)
+            private = message[len("private: "):]
+            if self.chats[nick] in self.groups:
+                self.groups[self.chats[nick]].remove(nick)
+            self.chats[nick] = 'p' + private
+            message = f"You have joined a private chat with {private}.\n"
+            self.send_to_one(message, nick)
+            text = f"{nick} has joined a private chat with you.\n"
+            self.send_to_chat(text, nick)
+        else:
+            self.send_to_chat(text, nick)
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((server_address, server_port))
-server_socket.listen()
-print(server_socket, file=sys.stderr)
+    def disconnect(self, nick):
+        if nick in self.clients:
+            text = f"{nick} has left the chat!"
+            self.send_to_chat(text, nick)
+            message = 'exit'
+            self.send_to_one(message, nick)
+            self.clients[nick].close()
+            if self.chats[nick] in self.groups:
+                self.groups[chats[nick]].remove(nick)
+            del self.chats[nick]
+            del self.clients[nick]
 
-clients = {}
-chats = {}
-groups = {'all' : set()}
+    def send_to_chat(self, message, nick):
+        if self.chats[nick] in self.groups:
+            for client in self.groups[self.chats[nick]]:
+                self.send_to_one(message, client)
+        else:
+            private = self.chats[nick][len('p'):]
+            if private in self.clients:
+                self.send_to_one(message, private)
 
-try: 
-    while True:
-        readable, _, _ = select.select([server_socket] + list(clients.values()), [], [])
+    def send_to_one(self, message, nick):
+        self.clients[nick].send(message.encode('utf-8'))
 
-        for trigger_socket in readable:
-            if trigger_socket == server_socket:
-                client_socket, client_address = server_socket.accept()
-                nick = verify_nick(client_socket)
-                if nick:
-                    print(f"[*] Accepted connection from {client_address[0]}:{client_address[1]}", file=sys.stderr)
-            else: # Receive message from existing client
-                nick = [key for key, value in clients.items() if value == trigger_socket][0]
-                message = trigger_socket.recv(1024).decode('utf-8')
-                continue_conversation(message, nick)
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.server_socket = None
+        self.clients = {}
+        self.chats = {}
+        self.groups = {'all': set()}
 
-except KeyboardInterrupt:
-    print("Keyboard interrupt received. Exiting server.", file=sys.stderr)
-finally:
-    for nick in clients.copy():
-        disconnect(nick)
-    server_socket.close()
 
+if __name__ == "__main__":
+    server_address = "0.0.0.0"
+    server_port = int(sys.argv[1])
+    chat_server = ChatServer(server_address, server_port)
+    chat_server.start()
