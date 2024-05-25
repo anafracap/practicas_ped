eb6fccb0be223cbf31ce6d2a1106d9345b2b41f5
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2024-05-25 20:54:37 +0200

    P2, ana, ap2, corregida ortografía

--- p2/ana/aplicacion2/cliente_servidor_hora.py
+++ p2/ana/aplicacion2/cliente_servidor_hora.py
@@ -4,18 +4,18 @@ class Hora:
     def __init__(self):
         self.rdC, self.wdS = os.pipe()  # Server to client pipe
 
-    def start(self, number_of_iteration):
+    def start(self, number_of_iterations):
         pid = os.fork()
 
         if pid:  # Parent - server
-            self.run_server(number_of_iteration)
+            self.run_server(number_of_iterations)
         else:  # Child - client
             self.run_client()
 
-    def run_server(self, number_of_iteration):
+    def run_server(self, number_of_iterations):
         os.close(self.rdC)
 
-        for i in range(number_of_iteration):
+        for i in range(number_of_iterations):
             date = datetime.datetime.now().strftime('%c')
             os.write(self.wdS, date.encode('utf8'))
 
@@ -30,5 +30,5 @@ class Hora:
 
 if __name__ == "__main__":
     hora = Hora()
-    number_of_iteration = int(sys.argv[1])
-    hora.start(number_of_iteration)
+    number_of_iterations = int(sys.argv[1])
+    hora.start(number_of_iterations)
