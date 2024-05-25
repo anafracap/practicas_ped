3391a90e442ddca2106de959bd28a01c87c19896
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2024-05-25 20:48:23 +0200

    P3, ana, ap1, refactor a clases y cleanup

--- p3/ana/aplicacion1/cliente_ap1.py
+++ p3/ana/aplicacion1/cliente_ap1.py
@@ -1,20 +1,31 @@
-import os, sys
+import sys
 
-sys.argv[0] = "cli2"
+class Client:
+    def __init__(self, server_fifo, client_fifo):
+        self.server_fifo = server_fifo
+        self.client_fifo = client_fifo
 
-serverWrites = "/tmp/p3_ap1_server_writes_ped4_ana"
-clientWrites = "/tmp/p3_ap1_client_writes_ped4_ana"
+    def send_message(self, message):
+        with open(self.client_fifo, "wb") as wc:
+            wc.write(message.encode('utf8'))
 
-#message = "./ejemplo.txt"
-#message = "/etc/services"
-#message = "/bin/sh"
-message = sys.argv[1]
-with open(clientWrites, "wb") as wc:
-    wc.write(message.encode('utf8'))
+    def receive_message(self):
+        with open(self.server_fifo, "rb") as rc:
+            while True:
+                byteLine = rc.read()
+                if not byteLine:
+                    break
+                sys.stdout.buffer.write(byteLine)
+    
+    def start(self, message):
+        client.send_message(message)
+        client.receive_message()
 
-with open(serverWrites, "rb") as rc:
-    while True:
-        byteLine = rc.read()
-        if not byteLine:
-            break
-        sys.stdout.buffer.write(byteLine)
+
+if __name__ == "__main__":
+    serverWrites = "/tmp/p3_ap1_server_writes_ped4_ana"
+    clientWrites = "/tmp/p3_ap1_client_writes_ped4_ana"
+    message = sys.argv[1]
+    
+    client = Client(serverWrites, clientWrites)
+    client.start(message)
--- p3/ana/aplicacion1/servidor_ap1.py
+++ p3/ana/aplicacion1/servidor_ap1.py
@@ -1,24 +1,33 @@
-import os, sys
+import os
 
-sys.argv[0] = "serv2"
+class Server:
+    def __init__(self, server_fifo, client_fifo):
+        self.server_fifo = server_fifo
+        self.client_fifo = client_fifo
+        self._create_fifos()
 
-serverWrites = "/tmp/p3_ap1_server_writes_ped4_ana"
-clientWrites = "/tmp/p3_ap1_client_writes_ped4_ana"
+    def _create_fifos(self):
+        if not os.path.exists(self.client_fifo):
+            os.mkfifo(self.client_fifo)
+        if not os.path.exists(self.server_fifo):
+            os.mkfifo(self.server_fifo)
 
-if not os.path.exists(clientWrites):
-    os.mkfifo(clientWrites)
-
-if not os.path.exists(serverWrites):
-    os.mkfifo(serverWrites)
-while True:
-    with open(clientWrites, "rb") as rs:
-        path = rs.read().decode('utf8').strip()
-
-    with open(path, 'rb') as fileO:  
+    def start(self):
         while True:
-            content = fileO.read()
-            if not content:
-                fileO.close()
-                break
-            with open(serverWrites, "wb") as ws:
-                ws.write(content)
+            with open(self.client_fifo, "rb") as rs:
+                path = rs.read().decode('utf8').strip()
+
+            with open(path, 'rb') as fileO:  
+                while True:
+                    content = fileO.read()
+                    if not content:
+                        fileO.close()
+                        break
+                    with open(self.server_fifo, "wb") as ws:
+                        ws.write(content)
+
+if __name__ == "__main__":
+    serverWrites = "/tmp/p3_ap1_server_writes_ped4_ana"
+    clientWrites = "/tmp/p3_ap1_client_writes_ped4_ana"
+    server = Server(serverWrites, clientWrites)
+    server.start()
