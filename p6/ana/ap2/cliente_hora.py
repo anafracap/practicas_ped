5e0e27143504436fbaf68f53e9df995198c97507
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2024-05-26 15:00:30 +0200

    P6, ana, ap2, configurable numero peticiones

--- p6/ana/ap2/cliente_hora.py
+++ p6/ana/ap2/cliente_hora.py
@@ -2,12 +2,12 @@ import os, sys, socket
 
 sys.argv[0] = "cli6"
 
-server_address = sys.argv[1]
-server_port = int(sys.argv[2])
+server_address = sys.argv[2]
+server_port = int(sys.argv[3])
 
 pid = str(os.getpid())
 
-for i in range(10):
+for i in range(int(sys.argv[1])):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((server_address, server_port))
     client_socket.send(pid.encode())
